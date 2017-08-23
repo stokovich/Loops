73851570bd79728a3950ec8e4d58afeb9dabeea2
Stoyan Penchev <spenchev@gmail.com>
2017-08-23 11:19:06 +0300

    after first task

---
+++ Loops/From1ToN.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class NumFrom1ToN
+{
+    static void Main()
+    {
+        int NumN = int.Parse(Console.ReadLine());
+        int i = 1;
+        while (i <= NumN)
+        {
+            Console.Write("{0} ", i);
+            i++;
+        }
+    }
+}
