eeea7c1ef28194f1aa29088a193e3af995d07afa
Stoyan Penchev <spenchev@gmail.com>
2017-08-24 09:42:16 +0300

    before Matrix of Numbers

--- CalculateThird/CalculateThird.cs
+++ CalculateThird/CalculateThird.cs
@@ -1,9 +1,35 @@
 ﻿using System;
+using System.Numerics;
 
 class CalculateThird
 {
     static void Main()
     {
+        double NumN = double.Parse(Console.ReadLine());
+        double NumK = double.Parse(Console.ReadLine());
+        double NminusK = NumN - NumK;
+        BigInteger FactN = 1;
+        BigInteger FactK = 1;
+        BigInteger FactNK = 1;
 
+        for (int i = 1; i <= NumN; i++)
+        {
+            if (i <= NumK)
+            {
+                FactN *= i;
+                FactK *= i;
+            }
+            else
+            {
+                FactN *= i;
+            }
+        }
+
+        for (int i = 1; i <= NminusK; i++)
+        {
+            FactNK *= i;
+        }
+
+        Console.WriteLine(FactN / (FactK * FactNK));
     }
 }
---
+++ CatalanNumbers/CatalanNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+class CatalanNumbers
+{
+    static void Main()
+    {
+        byte NumN = byte.Parse(Console.ReadLine());
+        BigInteger NK = 1;
+        BigInteger K = 1;
+
+        for (int i = 2; i <= NumN; i++)
+        {
+            NK *= NumN + i;
+            K *= i;
+        }
+        Console.WriteLine(NK / K);
+    }
+}
