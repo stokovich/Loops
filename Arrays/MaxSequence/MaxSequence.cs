27a27af22834be6f7ff3bcc7f66c2e6808512b28
Stoyan Penchev <spenchev@gmail.com>
2017-10-18 12:59:28 +0300

    before Selection Sort

--- Arrays/MaxSequence/MaxSequence.cs
+++ Arrays/MaxSequence/MaxSequence.cs
@@ -6,6 +6,27 @@ class MaxSequence
     {
         int Number = int.Parse(Console.ReadLine());
         int[] MyArray = new int[Number];
-
+        int MaxCounter = 1;
+        int PrevMaxCounter = 0;
+        for (int i = 0; i < Number; i++)
+        {
+            MyArray[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i = 1; i < Number; i++)
+        {
+            if (MyArray[i - 1] < (MyArray[i]))
+            {
+                MaxCounter = MaxCounter + 1;
+            }
+            else
+            {
+                MaxCounter = 1;
+            }
+            if (MaxCounter > PrevMaxCounter)
+            {
+                PrevMaxCounter = MaxCounter;
+            }
+        }
+        Console.WriteLine(PrevMaxCounter);
     }
 }
---
+++ Arrays/SumOfK/SumOfK.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class SumOfK
+{
+    static void Main()
+    {
+        int NNum = int.Parse(Console.ReadLine());
+        int KNum = int.Parse(Console.ReadLine());
+        int[] MainArray = new int[NNum];
+        int MaxSum = 0;
+
+        for (int i = 0; i < NNum; i++)
+        {
+            MainArray[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(MainArray, new Comparison<int>((i1, i2) => i2.CompareTo(i1)));
+
+        for (int i = 0; i < KNum; i++)
+        {
+            MaxSum = MaxSum + MainArray[i];
+        }
+        Console.WriteLine(MaxSum);
+    }
+}
